1ceb64559d3d68c0f5c9c0fbfa0321f02b22f07f
Jiawei Lee <jiaweilee95@126.com>
2020-11-23 15:42:39 +0800

    chore: change some config

---
+++ src/components/Flexbox/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-box"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="less"></style>
