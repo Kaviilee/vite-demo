983a9ad62c9f6291f03f46c2a8e20a8284628d59
Jiawei Lee <jiaweilee95@126.com>
2020-11-23 15:58:02 +0800

    fix: add images

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-//- img(alt="Vue logo" src="./assets/logo.png")
+img(alt="Vue logo" src="./assets/logo.png")
 HelloWorld(msg="Hello Vue 3.0 + Vite!")
 router-view
 </template>
