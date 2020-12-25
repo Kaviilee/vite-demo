55109523c253e927b5811c605b3cdf36c505f098
Jiawei Lee <jiaweilee95@126.com>
2020-12-25 17:26:38 +0800

    chore: 新的复制方式

--- src/components/Clipboard/index.tsx
+++ src/components/Clipboard/index.tsx
@@ -1,36 +1,51 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import './index.less'
 
 const Clipboard = defineComponent({
   setup() {
+    const help = ref('单击复制')
     const copyActive = () => {
-      const copy = document.getElementById('copy') as HTMLTextAreaElement;
+      const copy = document.getElementById('copy') as HTMLInputElement;
       console.log(copy?.value)
       // copy.select();
-      // const range = document.createRange();
-      // window.getSelection()?.removeAllRanges();
-      // range.selectNode(copy);
-      // window.getSelection()?.addRange(range);
+      const range = document.createRange();
+      window.getSelection()?.removeAllRanges();
+      range.selectNode(copy);
+      window.getSelection()?.addRange(range);
 
-      if (copy) {
-        navigator.clipboard.writeText(copy.value).then(() => {
-          console.log('复制成功')
-        }).catch(err => {
-          console.error(err)
-        })
+      const success = document.execCommand('copy')
+
+      if (success) {
+        help.value = '已将文本复制到剪贴板'
+      } else {
+        help.value = '复制失败'
       }
+
+      // if (copy) {
+      //   navigator.clipboard.writeText(copy.value).then(() => {
+      //     help.value = '复制成功'
+      //   }).catch(err => {
+      //     help.value = '复制失败'
+      //     console.error(err)
+      //   })
+      // }
     }
 
     return {
-      copyActive
+      copyActive,
+      help,
     }
   },
 
   render() {
     return (
-      <>
-        <textarea name="" id="copy" rows={10}></textarea>
-        <button onClick={this.copyActive}>copy</button>
-      </>
+      <div class="copy-container">
+        <pre
+          id="copy"
+          class="pre"
+        >This is a test</pre>
+        <div class="help" onClick={this.copyActive}>{this.help}</div>
+      </div>
     )
   }
 
