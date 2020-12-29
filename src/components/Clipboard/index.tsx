5d138f8088e3af4ae7d487c0c4159dd630125276
Kaviilee <jiaweilee@126.com>
2020-12-29 23:30:28 +0800

    fix: new methods of copy

--- src/components/Clipboard/index.tsx
+++ src/components/Clipboard/index.tsx
@@ -4,11 +4,23 @@ import './index.less'
 const Clipboard = defineComponent({
   setup() {
     const help = ref('单击复制')
+
+    const initClipboard = () => {
+      document.addEventListener('copy', (e) => {
+        e.clipboardData?.setData('text/plain', 'Hello world')
+
+        e.preventDefault()
+      })
+    }
+
     const copyActive = () => {
       const copy = document.getElementById('copy') as HTMLInputElement;
-      console.log(copy?.value)
       // copy.select();
-      const range = document.createRange();
+
+      /** 1
+       * document.execCommand('copy')
+       */
+      /* const range = document.createRange();
       window.getSelection()?.removeAllRanges();
       range.selectNode(copy);
       window.getSelection()?.addRange(range);
@@ -19,16 +31,25 @@ const Clipboard = defineComponent({
         help.value = '已将文本复制到剪贴板'
       } else {
         help.value = '复制失败'
-      }
+      } */
 
-      // if (copy) {
-      //   navigator.clipboard.writeText(copy.value).then(() => {
-      //     help.value = '复制成功'
-      //   }).catch(err => {
-      //     help.value = '复制失败'
-      //     console.error(err)
-      //   })
-      // }
+      /** 2
+       * navigator.clipboard
+       */
+      /* if (copy) {
+        navigator.clipboard.writeText(copy.value).then(() => {
+          help.value = '复制成功'
+        }).catch(err => {
+          help.value = '复制失败'
+          console.error(err)
+        })
+      } */
+
+      /** 3
+       * clipboard Event API
+       */
+      const copyEvent = new ClipboardEvent('copy')
+      copyEvent.clipboardData?.items
     }
 
     return {
